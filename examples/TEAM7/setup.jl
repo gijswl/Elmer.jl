c62d2eaba9419258705145e202ddf1e318ab0762
gwlagerweij <gijswlagerweij@gmail.com>
2025-08-25 16:38:34 +0200

    Moved config files into example directory

--- examples/TEAM7/setup.jl
+++ examples/TEAM7/setup.jl
@@ -6,16 +6,16 @@ frequency = 50
 simulation = Simulation(7, Elmer.CoordinateCartesian(), Elmer.SimulationSteady(), 1, OrderedDict("Frequency" => frequency))
 sif = SolverInformationFile("case", simulation, data_path="examples/TEAM7/simdata/", mesh_db="team7")
 
-solver_coil = load_solver!(sif, "CoilSolver", Elmer.ExecBeforeAll(), "examples/solvers.yml")
-solver_mgharm = load_solver!(sif, "MGDynHarm", Elmer.ExecAlways(), "examples/solvers.yml")
-solver_mgpost = load_solver!(sif, "MGDynPost", Elmer.ExecAlways(), "examples/solvers.yml")
-solver_result = load_solver!(sif, "ResultOutput", Elmer.ExecAfterStep(), "examples/solvers.yml")
+solver_coil = load_solver!(sif, "CoilSolver", Elmer.ExecBeforeAll(), "examples/TEAM7/solvers.yml")
+solver_mgharm = load_solver!(sif, "MGDynHarm", Elmer.ExecAlways(), "examples/TEAM7/solvers.yml")
+solver_mgpost = load_solver!(sif, "MGDynPost", Elmer.ExecAlways(), "examples/TEAM7/solvers.yml")
+solver_result = load_solver!(sif, "ResultOutput", Elmer.ExecAfterStep(), "examples/TEAM7/solvers.yml")
 
 eq_coil = add_equation!(sif, "coil", [solver_coil, solver_mgharm, solver_mgpost, solver_result])
 eq_domain = add_equation!(sif, "domain", [solver_mgharm, solver_mgpost, solver_result])
 
-mat_air = Elmer.load_material!(sif, "Air", "examples/materials.yml")
-mat_aluminium = Elmer.load_material!(sif, "Aluminium", "examples/materials.yml")
+mat_air = Elmer.load_material!(sif, "Air", "examples/TEAM7/materials.yml")
+mat_aluminium = Elmer.load_material!(sif, "Aluminium", "examples/TEAM7/materials.yml")
 
 body_coil = add_body!(sif, "Coil", [2]; equation=eq_coil, material=mat_air)
 body_plate = add_body!(sif, "Plate", [1]; equation=eq_domain, material=mat_aluminium)
