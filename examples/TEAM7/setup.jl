80ef989f79835ef1498542abe630462a1f3ea61a
gwlagerweij <gijswlagerweij@gmail.com>
2025-08-26 15:10:24 +0200

    Update solver settings for TEAM7 example

--- examples/TEAM7/setup.jl
+++ examples/TEAM7/setup.jl
@@ -2,35 +2,50 @@ using Elmer
 using OrderedCollections
 
 # Parameter definition
-frequency = 50
+frequency = [50, 200]
 
-# Set up simulation
-simulation = Simulation(7, Elmer.CoordinateCartesian(), Elmer.SimulationSteady(), 1, OrderedDict("Frequency" => frequency))
-sif = SolverInformationFile("case", simulation, data_path="examples/TEAM7/simdata/", mesh_db="team7")
+for (i, f) ∈ enumerate(frequency)
+    # Set up simulation
+    simulation = Simulation(7, Elmer.CoordinateCartesian(), Elmer.SimulationSteady(), 1, OrderedDict("Frequency" => f))
 
-solver_coil = load_solver!(sif, "CoilSolver", Elmer.ExecBeforeAll(), "examples/TEAM7/solvers.yml")
-solver_mgharm = load_solver!(sif, "MGDynHarm", Elmer.ExecAlways(), "examples/TEAM7/solvers.yml")
-solver_mgpost = load_solver!(sif, "MGDynPost", Elmer.ExecAlways(), "examples/TEAM7/solvers.yml")
-solver_result = load_solver!(sif, "ResultOutput", Elmer.ExecAfterStep(), "examples/TEAM7/solvers.yml")
+    data_path = joinpath(@__DIR__, "simdata\\")
+    solvers_db = joinpath(@__DIR__, "solvers.yml")
+    materials_db = joinpath(@__DIR__, "materials.yml")
 
-eq_coil = add_equation!(sif, "coil", [solver_coil, solver_mgharm, solver_mgpost, solver_result])
-eq_domain = add_equation!(sif, "domain", [solver_mgharm, solver_mgpost, solver_result])
+    sif = SolverInformationFile("case$i", simulation, data_path=data_path, mesh_db="team7")
 
-# Define physical bodies
-mat_air = Elmer.load_material!(sif, "Air", "examples/TEAM7/materials.yml")
-mat_aluminium = Elmer.load_material!(sif, "Aluminium", "examples/TEAM7/materials.yml")
+    solver_coil = load_solver!(sif, "CoilSolver", Elmer.ExecBeforeAll(), solvers_yml)
+    solver_mgharm = load_solver!(sif, "MGDynHarm", Elmer.ExecAlways(), solvers_yml)
+    post_mgharm = load_solver!(sif, "MGDynPost", Elmer.ExecAlways(), solvers_yml)
+    save_scalars = load_solver!(sif, "SaveScalars", Elmer.ExecAlways(), solvers_yml)
+    save_result = load_solver!(sif, "ResultOutput", Elmer.ExecAfterStep(), solvers_yml)
+    update_solver_data!(sif, save_scalars, "Filename", "scalars$i.dat")
+    update_solver_data!(sif, save_result, "Output File Name", "case$i")
 
-body_coil = add_body!(sif, "Coil", [2]; equation=eq_coil, material=mat_air)
-body_plate = add_body!(sif, "Plate", [1]; equation=eq_domain, material=mat_aluminium)
-body_domain = add_body!(sif, "Domain", [3]; equation=eq_domain, material=mat_air)
+    eq_coil = add_equation!(sif, "coil", [solver_coil, solver_mgharm, post_mgharm, save_scalars])
+    eq_domain = add_equation!(sif, "domain", [solver_mgharm, post_mgharm])
 
-bc_inf = add_boundary_condition!(sif, "Inf", data=OrderedDict("AV {e}" => 0), [1])
+    # Define physical bodies
+    mat_air = Elmer.load_material!(sif, "Air", materials_db)
+    mat_aluminium = Elmer.load_material!(sif, "Aluminium", materials_db)
 
-component = add_component!(sif, "Coil", data=OrderedDict("Coil Type" => "\"Stranded\"", "Number of Turns" => 1, "Desired Coil Current" => -2742); master_bodies=[body_coil])
+    body_coil = add_body!(sif, "Coil", [2]; equation=eq_coil, material=mat_air)
+    body_plate = add_body!(sif, "Plate", [1]; equation=eq_domain, material=mat_aluminium)
+    body_domain = add_body!(sif, "Domain", [3]; equation=eq_domain, material=mat_air)
 
-# Write SIF & run
-Elmer.write(sif)
-Elmer.write_startinfo(sif)
+    bc_inf = add_boundary_condition!(sif, "Inf", [1]; data=OrderedDict("AV {e}" => 0))
 
-# Elmer.elmergrid_gmsh(sif.data_path, "team7.msh")
-Elmer.run_elmer_solver(sif)
+    component = add_component!(sif, "Coil", data=OrderedDict(
+            "Coil Type" => "Stranded",
+            "Number of Turns" => 1,
+            "Desired Coil Current" => 2742,
+            "Coil Use W Vector" => true,
+            "W Vector Variable Name" => "String \"CoilCurrent e\""); master_bodies=[body_coil])
+
+    # Write SIF & run
+    Elmer.write(sif)
+    #Elmer.write_startinfo(sif)
+
+    # Elmer.elmergrid_gmsh(sif.data_path, "team7.msh")
+    Elmer.run_elmer_solver(sif)
+end
